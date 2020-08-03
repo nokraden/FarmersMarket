a5b08115f4895a3b07a0599abc79ff58fbdb2edf
Robert Martin <nokraden@gmail.com>
2020-08-03 15:19:08 -0700

    Update Product.php

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -11,7 +11,7 @@ class Product extends Model
     //const UPDATED_AT = 'Updated_At';	
 	
 	//protected $primaryKey = 'Product_SID';
-	
+	//can also add protedctd fields if needed
 	protected $fillable = [
         'market_id',
         'vendor_id',
